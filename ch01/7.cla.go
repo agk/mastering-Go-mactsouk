57c4756f106b3327ae46c437e3190af525375451
Alexey Kucheryavenko <kucheryavenko.alexey@gmail.com>
2026-02-06 12:25:24 +0300

    refact

--- ch01/7.cla.go
+++ ch01/7.cla.go
@@ -12,31 +12,31 @@ func main() {
 		fmt.Println("Need one or more arguments!")
 		return
 	}
-	
+
 	var min, max float64
 	for i := 1; i < len(arguments); i++ {
 		n, err := strconv.ParseFloat(arguments[i], 64)
 		if err != nil {
 			continue
 		}
-		
+
 		if i == 1 {
 			min = n
 			max = n
 			continue
 		}
-		
+
 		if n < min {
 			min = n
 		}
-		
+
 		if n > max {
 			max = n
 		}
-		
+
 	}
-	
+
 	fmt.Println("Min: ", min)
-	fmt.Println("Mzx: ", max)
-	
+	fmt.Println("Max: ", max)
+
 }
